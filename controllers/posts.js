5e21015ca7a8f377a306b2870a44f9906befc665
Martin Cosimano <martincosimano@hotmail.com>
2023-03-01 14:27:05 -0300

    now at the moment to make a post, if any of the fields is empty or the spotify api can found any data on the search, it will show the error below "add a post"

--- controllers/posts.js
+++ controllers/posts.js
@@ -4,11 +4,10 @@ const { getApiToken, searchTrack } = require("../services/spotify");
 
 // Post controller functions
 module.exports = {
-  getProfile: async (req, res) => { 
-    console.log(req.user)
+  getProfile: async (req, res) => {
     try {
       const posts = await Post.find({ user: req.user.id });
-      res.render("profile.ejs", { posts: posts, user: req.user });
+      res.render("profile.ejs", { posts: posts, user: req.user, flash: req.flash() });
     } catch (err) {
       console.log(err);
     }
@@ -31,37 +30,48 @@ module.exports = {
   },
   createPost: async (req, res) => {
     try {
-      const { songName, artistName } = req.body;
-  
-      // Check if the required fields are filled in
-      if (!songName || !artistName) {
-        throw new Error('Song name and artist name are required');
-      }
-  
-      const token = await getApiToken();
-      const trackData = await searchTrack(songName, artistName, token);
+        const { songName, artistName, title, caption } = req.body;
 
-      if (!trackData?.tracks?.items?.length) {
-        throw new Error('No track data found for this song and artist');
-      }
-      
-      const post = await Post.create({
-        title: req.body.title,
-        caption: req.body.caption,
-        likes: 0,
-        songName,
-        artistName,
-        spotifyTrackId: trackData.tracks.items[0].id,
-        user: req.user?.id, // Check if req.user exists before accessing its properties
-      });
-  
-      console.log('Post has been added!');
-      res.redirect('/profile');
+        // Check if the required fields are filled in
+        if (!title) {
+            throw new Error('Title is required');
+        }else if(!caption) {
+          throw new Error('Caption is required')
+        }else if(!songName) {
+          throw new Error('Song name is required')
+        }else if(!artistName){
+          throw new Error('Artist name is required')
+        }
+
+        const token = await getApiToken();
+        const trackData = await searchTrack(songName, artistName, token);
+
+        if (!trackData?.tracks?.items?.length) {
+            throw new Error('No track data found for this song and artist');
+        }
+
+        const post = await Post.create({
+            title,
+            caption,
+            likes: 0,
+            songName,
+            artistName,
+            spotifyTrackId: trackData.tracks.items[0].id,
+            user: req.user?.id,
+        });
+
+        console.log('Post has been added!');
+        res.redirect('/profile');
     } catch (err) {
-      console.log(err);
-      res.status(400).send(err.message); // Send an error response to the client
+        console.log(err);
+
+        // Set a flash message to display the error on the profile page
+        req.flash('error', err.message);
+
+        // Redirect to the profile page
+        res.redirect('/profile');
     }
-  },
+},
   likePost: async (req, res) => {
     try {
       await Post.findOneAndUpdate(
