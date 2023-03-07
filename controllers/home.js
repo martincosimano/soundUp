61b97c26a00e7e76aeb4938688706baa7eb44dd0
Martin Cosimano <martincosimano@hotmail.com>
2023-03-06 21:25:02 -0300

    index now can handle variable currentUserID

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,5 @@
 module.exports = {
   getIndex: (req, res) => {
-    res.render("index.ejs", { user: req.user });
+    res.render("index.ejs", { user: req.user, currentUserID: req.user ? req.user._id : null, });
   },
 };
